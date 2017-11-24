9de2f10edcb18f1e2286d23afaeed90e5e1e19e1
Ye Min Naing <thargyi74@gmail.com>
2017-11-24 14:24:30 +0630

    make screen Transition animation

--- app/src/main/java/com/example/android/lollipopactivitytransitions/MainActivity.java
+++ app/src/main/java/com/example/android/lollipopactivitytransitions/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.android.lollipopactivitytransitions;
 
 import android.content.Intent;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -32,6 +35,27 @@ public class MainActivity extends AppCompatActivity {
                         Intent intent = new Intent(MainActivity.this, DetailActivity.class);
                         intent.putExtra(DetailActivity.ID, Contact.CONTACTS[position].getId());
                         startActivity(intent);
+
+
+
+                        ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                                // the context of the activity
+                                MainActivity.this,
+
+                                // For each shared element, add to this method a new Pair item,
+                                // which contains the reference of the view we are transitioning *from*,
+                                // and the value of the transitionName attribute
+                                new Pair<View, String>(view.findViewById(R.id.CONTACT_circle),
+                                        getString(R.string.transition_name_circle)),
+                                new Pair<View, String>(view.findViewById(R.id.CONTACT_name),
+                                        getString(R.string.transition_name_name)),
+                                new Pair<View, String>(view.findViewById(R.id.CONTACT_phone),
+                                        getString(R.string.transition_name_phone))
+                        );
+                        ActivityCompat.startActivity(MainActivity.this, intent, options.toBundle());
+
+
+
                     }
                 }));
     }
